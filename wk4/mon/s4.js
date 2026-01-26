3c28393a1bedabd73bfecf6f14a48a273293e668
Prof Blanc <ben.blanc@georgebrown.ca>
2026-01-26 13:15:43 -0500

    Class Code Jan 26 @ 13h15

---
+++ wk4/mon/s4.js
@@ -0,0 +1,23 @@
+//named funtion
+function greet1(name){
+    return "Hello, " + name;
+}
+
+//function expression
+const greet2 = function(name){
+    return "Howdy, " + name;
+}
+
+//arrow funtion
+const greet3 = (n) => "Salutations" + n;
+
+const greet4 = (n) => { n += n; return "Salutations" + n;}
+
+let fruits = ["Apple", "Banana", "Cherry"];
+/*
+    push(elmentOne [,otherElements])
+    pop()
+    shift()
+    unshift(elementOne[, otherElements])
+
+*/
