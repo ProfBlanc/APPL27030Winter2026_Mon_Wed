f50212778cd024932c9fcfdc5b96115de9fb8eb6
Prof Blanc <ben.blanc@georgebrown.ca>
2026-02-02 11:22:38 -0500

    Class Code Feb 2, 2026 @ 11h20

---
+++ wk5/mon/intro.php
@@ -0,0 +1,22 @@
+<?php 
+
+// single-line comment
+# another way to create a single-line comment
+/* multiline comment */
+
+//Personal Home Page
+//PHP: PHP HyperText Preprocessor 
+
+$name = "Sheridan";
+$age = 100;
+$temperature = -20.5;
+$isHavingFun = true;
+$marks = [90, 80, 70, 85, 95];
+
+print("Hello there ". $name. "<br/>"); //. character = concatenation = merging values
+echo "Howdy, ", 
+$name, 
+"! It is ". $temperature . " degrees Celsius outside";
+
+
+?>
